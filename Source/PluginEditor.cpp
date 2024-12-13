6e94c4029ae723a44da646b56cc42dccd9685e93
MSI\2220412 <z.ryu.d.0822@gmail.com>
2024-12-13 12:05:58 +0900

    一応終わった。UIと連動するようになった。 最後の方はやってないし、コード解説も飛ばした。 DAWでプラグインを開いたときにDAWのショートカット効かなくなるかも。

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,16 @@ GainPanTutorialAudioProcessorEditor::GainPanTutorialAudioProcessorEditor (GainPa
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    
+    addAndMakeVisible(webComponent);
+#if 0
+    webComponent.goToURL("http://localhost:5173/");
+#else
+    webComponent.goToURL(juce::WebBrowserComponent::getResourceProviderRoot());
+#endif
+
+	setSize(170, 650);
+    //setSize (400, 300);
 }
 
 GainPanTutorialAudioProcessorEditor::~GainPanTutorialAudioProcessorEditor()
@@ -28,13 +37,65 @@ void GainPanTutorialAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void GainPanTutorialAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+
+    webComponent.setBounds(getLocalBounds());
+
+}
+
+std::optional<juce::WebBrowserComponent::Resource>
+GainPanTutorialAudioProcessorEditor::getResource(const juce::String& url)
+{
+    const auto urlToRetrive = url == "/" 
+                                    ? juce::String{ "index.html" }
+                                    : url.fromFirstOccurrenceOf("/", false, false);
+    
+    static auto streamZip = juce::MemoryInputStream(juce::MemoryBlock(BinaryData::assets_zip, BinaryData::assets_zipSize), true);
+
+    static juce::ZipFile archive{ streamZip };
+
+    if (auto* entry = archive.getEntry(urlToRetrive))
+    {
+        auto entryStream = rawToUniquePtr(archive.createStreamForEntry(*entry));
+        std::vector<std::byte> result((size_t)entryStream->getTotalLength());
+        entryStream->setPosition(0);
+        entryStream->read(result.data(), result.size());
+
+        auto mine = getMimeForCExtension(entry->filename.fromLastOccurrenceOf(".", false, false).toLowerCase());
+        return juce::WebBrowserComponent::Resource{ std::move(result), std::move(mine) };
+    }
+    return std::nullopt;
+}
+
+const char* GainPanTutorialAudioProcessorEditor::getMimeForCExtension(const juce::String& extension)
+{
+    static const std::unordered_map<juce::String, const char*>mineMap =
+    {
+        {{"htm"},"text/html"},
+        {{"html"},"text/html"},
+        {{"txt"},"text/plain"},
+        {{"jpg"},"image/jpeg"},
+        {{"jpeg"},"image/jpeg"},
+        {{"svg"},"image/svg+xml"},
+        {{"ico"},"image/vnd.microsoft.icon"},
+        {{"json"},"application/json"},
+        {{"png"},"image/png"},
+        {{"css"},"text/css"},
+        {{"map"},"application/json"},
+        {{"js"},"text/javascript"},
+        {{"woff2"},"font/woff2"}
+    };
+
+    if (const auto it = mineMap.find(extension.toLowerCase()); it != mineMap.end())
+    {
+        return it->second;
+    }
+
+    jassertfalse;
+    return "";
 }
